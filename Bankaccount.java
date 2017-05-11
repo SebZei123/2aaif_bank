d91530893cf7f18db66fcc4ce46a8c8616c64e17
Sebastian <afasdf@afas.com>
2017-05-11 19:55:18 +0200

    1Version1

---
+++ Bankaccount.java
@@ -0,0 +1,8 @@
+package at.spengergasse.bank.domain;
+
+import java.utils.*;
+
+public class Bank{
+	private ArrayList accounts;
+}
+
---
+++ sell_zeinzinger/Product.java
@@ -0,0 +1,15 @@
+public class Product{
+	private String name;
+	
+	public Product(){
+		name = "Coca Cola";
+	}
+	
+	public void setProduct(String product){
+		name = product;
+	}
+	
+	public String getProduct(){
+		return name;
+	}
+}
